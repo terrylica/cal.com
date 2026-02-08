7c42cde035cd8484cae40073e49d2c69877ffda0
hbjORbj <sldisek783@gmail.com>
2026-02-09 01:52:00 +0900

    trpc

--- packages/trpc/server/routers/viewer/i18n/i18n.handler.ts
+++ packages/trpc/server/routers/viewer/i18n/i18n.handler.ts
@@ -1,7 +1,5 @@
-import path from "node:path";
-import type { I18nInputSchema } from "./i18n.schema";
 import i18nConfig from "@calcom/config/next-i18next.config";
-import type { UserConfig } from "next-i18next";
+import type { I18nInputSchema } from "./i18n.schema";
 
 type I18nOptions = {
   input: I18nInputSchema;
@@ -10,11 +8,7 @@ type I18nOptions = {
 export const i18nHandler = async ({ input }: I18nOptions) => {
   const { locale } = input;
   const { serverSideTranslations } = await import("next-i18next/serverSideTranslations");
-  const config: UserConfig = {
-    ...i18nConfig,
-    localePath: path.resolve("./public/static/locales"),
-  };
-  const i18n = await serverSideTranslations(locale, ["common", "vital"], config);
+  const i18n = await serverSideTranslations(locale, ["common", "vital"], i18nConfig);
 
   return {
     i18n,
