d7dff6d38b85557001697d0039c9f26ef084b287
Ryukemeister <sahalrajiv6900@gmail.com>
2026-02-10 16:39:51 +0530

    cleanup

--- packages/features/eventtypes/components/tabs/availability/index.ts
+++
@@ -1,9 +0,0 @@
-export { EventAvailabilityTab } from "./EventAvailabilityTab";
-export type {
-  EventAvailabilityTabProps,
-  EventAvailabilityTabBaseProps,
-  EventAvailabilityTabCustomClassNames,
-  ScheduleQueryData,
-  TeamMember,
-  HostSchedulesQueryType,
-} from "./EventAvailabilityTab";
--- packages/features/eventtypes/components/tabs/setup/index.ts
+++
@@ -1,9 +0,0 @@
-export { EventSetupTab } from "./EventSetupTab";
-export type {
-  EventSetupTabProps,
-  EventSetupTabBaseProps,
-  EventSetupTabSlots,
-  EventSetupTabCustomClassNames,
-  LocationsSlotProps,
-  HostLocationsSlotProps,
-} from "./EventSetupTab";
